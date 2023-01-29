7a6b6fba1cb0020fffd5627b05a7bc98bf8dfdb7
chanwoo4267 <chanwoo4267@chanwoo4267ui-MacBookPro.local>
2023-01-29 18:54:13 +0900

    fix exit diff

--- execute/builtin_exit.c
+++ execute/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: chanwopa <chanwopa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 13:11:07 by chanwopa          #+#    #+#             */
-/*   Updated: 2023/01/28 12:15:37 by chanwopa         ###   ########seoul.kr  */
+/*   Updated: 2023/01/29 18:52:29 by chanwopa         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,6 @@ void	builtin_exit(t_list *list)
 		system_error("builtin_exit", "malloc error", 1);
 	else if (!command[1])
 		exit(g_status.global_exit_status);
-	else if (command[2])
-	{
-		print_error(command[0], "too many arguments", NULL, NO);
-		free_strs(command);
-		return ;
-	}
 	if (!ft_isnumeric(command[1]))
 	{
 		errno = 22;
@@ -43,7 +37,6 @@ void	builtin_exit(t_list *list)
 
 /*
 	exit 단일로 올 경우 현재 global status로 그대로 exit해준다.
-	exit 뒤에 인자가 2개 이상 올 경우 오류를 출력해주고 exit하지 않는다.
 	exit 뒤에 인자가 1개인 경우, 뒤의 인자가 숫자가 아니라면 
 	errno = 22 (invalid argument) 오류 출력 후 exit(255)
 	정상적인 숫자 인자라면 해당 수를 256으로 나눈 나머지로, 음수면 양수로 변환해서
