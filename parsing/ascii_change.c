eb6dad225124cdc5484df663b5cf36ed2571d4dd
Sehoon Jung <sehjung@cx1r3s2.42seoul.kr>
2023-01-29 19:43:23 +0900

    230129: fix quote 1. 문자열 앞, 뒤, 사이에 quote가 올경우에 대한 예외처리 수정

--- parsing/ascii_change.c
+++ parsing/ascii_change.c
@@ -6,7 +6,7 @@
 /*   By: sehjung <sehjung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 19:21:05 by sehjung           #+#    #+#             */
-/*   Updated: 2023/01/29 17:07:15 by sehjung          ###   ########seoul.kr  */
+/*   Updated: 2023/01/29 19:35:40 by sehjung          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,14 +61,26 @@ static void	single_quote(char *str)
 static void	del_quote(char **str)
 {
 	int	i;
+	int	j;
 
 	i = 0;
 	while (str[i])
 	{
-		if (str[i][0] == '\'')
-			single_quote(str[i]);
-		else if (str[i][0] == '\"')
-			double_quote(str[i]);
+		j = 0;
+		while (str[i][j])
+		{
+			if (str[i][j] == '\'')
+			{
+				single_quote(str[i]);
+				break ;
+			}
+			else if (str[i][j] == '\"')
+			{
+				double_quote(str[i]);
+				break ;
+			}
+			j++;
+		}
 		i++;
 	}
 }
